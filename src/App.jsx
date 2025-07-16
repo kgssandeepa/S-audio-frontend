02794e322d2565926ba252471fda678a9f63af00
kgssandeepa <sonalsandeepa08@gmail.com>
2025-07-16 13:05:53 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ProductCard from './components/productCard'
+
+function App() {
+ 
+
+  return (
+    <>
+ <ProductCard name="Audio Setup"/>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,13 @@
+import "./ProductCard.css";
+
+export default function ProductCard(Props){
+   return(
+    <div>
+        <img src="https://www.raypcb.com/wp-content/uploads/2022/09/Audio-Components-and-Accessories.jpg"/>
+        <span>JBL AUDIO MINI 3</span>
+        <span>LKR.2250/-</span>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo ea sint velit suscipit tenetur? Excepturi accusantium perferendis reprehenderit? Obcaecati sunt explicabo eius. Quasi hic nobis dicta, in totam aperiam ut.</p>
+    </div>
+   )
+}
+
